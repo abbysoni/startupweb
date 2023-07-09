39c11c0ce47090902d87794db17bba9cf007cf50
Testabby <a9094855@gmail.com>
2023-07-09 16:05:26 +0530

    print directly from app.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import Midhome from "./Pages/Homepage/Midhome";
 import Footer from "./Pages/Homepage/Footer";
 
 
+// function App() {
+//   return (
+//     <div className="App">
+//     <Header/>
+//     <Tophome/>
+//     <Midhome/>
+//     <Footer/>
+
+//     </div>
+//   );
+// }
+// export default App;
+import React from 'react'
+
 function App() {
   return (
-    <div className="App">
-    <Header/>
-    <Tophome/>
-    <Midhome/>
-    <Footer/>
-
+    <div>the text is shown directly from app.js file
     </div>
-  );
+  )
 }
-export default App;
+
+export default App
