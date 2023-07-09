d6c060a3be3454730c2f36aaf89adfc19eba4bd6
Testabby <a9094855@gmail.com>
2023-07-09 15:46:28 +0530

    none 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./Pages/Homepage/Header";
 import Tophome from "./Pages/Homepage/Tophome";
 import Midhome from "./Pages/Homepage/Midhome";
 import Footer from "./Pages/Homepage/Footer";
-import Home from './Pages/Home';
+
 
 function App() {
   return (
