295a502ff0c10715dad790f042e4b1a24f9fc1bc
Testabby <a9094855@gmail.com>
2023-07-10 12:19:02 +0530

    app.js with full code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-// import './App.css';
-// import Header from "./Pages/Homepage/Header";
-// import Tophome from "./Pages/Homepage/Tophome";
-// import Midhome from "./Pages/Homepage/Midhome";
-// import Footer from "./Pages/Homepage/Footer";
+import './App.css';
+import Header from "./Pages/Homepage/Header";
+import Tophome from "./Pages/Homepage/Tophome";
+import Midhome from "./Pages/Homepage/Midhome";
+import Footer from "./Pages/Homepage/Footer";
 
-
-// function App() {
-//   return (
-//     <div className="App">
-//     <Header/>
-//     <Tophome/>
-//     <Midhome/>
-//     <Footer/>
-
-//     </div>
-//   );
-// }
-// export default App;
-import React from 'react'
 
 function App() {
   return (
-    <div>the text is shown directly from app.js file
-    </div>
-  )
-}
+    <div className="App">
+    <Header/>
+    <Tophome/>
+    <Midhome/>
+    <Footer/>
 
-export default App
+    </div>
+  );
+}
+export default App;
+// import React from 'react'
+
+// function App() {
+//   return (
+//     <div>the text is shown directly from app.js file
+//     </div>
+//   )
+// }
+
+// export default App
