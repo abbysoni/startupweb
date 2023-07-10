9040fe24f884c53191bde4a10e8f00d43352ba81
Testabby <a9094855@gmail.com>
2023-07-10 12:30:22 +0530

    header changed

--- src/Pages/Homepage/Header.jsx
+++ src/Pages/Homepage/Header.jsx
@@ -4,16 +4,16 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <a to="/">
+      {/* <a to="/">
         <img className="logo" src="/logo.png" alt="Logo" />
-      </a>
+      </a> */}
       <nav className="navigation">
         <a to="/become-a-professional">Become a Professional</a>
         <a to="/help">Helps</a>
         <a to="/login">Login</a>
         <a to="/sign-up">Sign Up</a>
       </nav>
-      //dev branch changes done on addH, not in dev or master
+     
     </header>
   );
 };
